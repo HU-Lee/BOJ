9fb76c97084b184ab773d593744dca40840fa990
LHU <haulrest@gmail.com>
2024-02-11 18:27:19 +0900

    [Bronze V] Title: 고양이, Time: 112 ms, Memory: 9272 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.ts
@@ -0,0 +1,11 @@
+function main() {
+  // const input = fs.readFileSync("1.txt").toString().split("\n");
+  // const input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+  console.log("\\    /\\");
+  console.log(" )  ( ')");
+  console.log("(  /  )");
+  console.log(" \\(__)|");
+}
+
+main();
