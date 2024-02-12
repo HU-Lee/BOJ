a518c6400f8967cf65702cb2f6a14f23fa373960
LHU <haulrest@gmail.com>
2024-02-12 12:56:38 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 796 ms, Memory: 6772 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+
+	defer writer.Flush()
+
+	var n int
+	fmt.Fscanln(reader, &n)
+
+	for i := 0; i < n; i++ {
+		var a,b int
+		fmt.Fscanln(reader, &a, &b)
+		fmt.Fprintln(writer, a+b)
+	}
+}
