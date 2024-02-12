1a5f0d0768b327759442118daf54ecce06fb702a
LHU <haulrest@gmail.com>
2024-02-12 13:00:21 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 124 ms, Memory: 9320 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.ts
@@ -0,0 +1,16 @@
+import fs = require("fs");
+
+function main() {
+  const input = fs.readFileSync("/dev/stdin").toString().split(" ");
+  const a = Number(input[0])
+  const b = Number(input[1])
+  if (a < b) {
+    console.log("<")
+  } else if (a > b) {
+    console.log(">")
+  } else {
+    console.log("==")
+  }
+}
+
+main();
