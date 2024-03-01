6fc6af1376ab6a477a52df4f558391f419b7e34a
LHU <haulrest@gmail.com>
2024-03-01 13:15:13 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 4 ms, Memory: 872 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+
+	defer writer.Flush()
+
+	var n int
+	fmt.Fscanln(reader, &n)
+
+	fmt.Fprintln(writer, strings.Repeat("long ", n/4) + "int")
+}
