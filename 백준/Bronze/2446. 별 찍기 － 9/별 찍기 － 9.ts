4a7fcc52859e88aa7fb3209d6a91a2f989719314
LHU <haulrest@gmail.com>
2024-03-03 14:20:40 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 136 ms, Memory: 9680 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.ts
@@ -0,0 +1,14 @@
+import fs = require("fs");
+const path = process.platform == "linux" ? "/dev/stdin" : "1.txt";
+
+function main() {
+  const n = Number(fs.readFileSync(path).toString().trim());
+  for (let i = n - 1; i > 0; i--) {
+    console.log(" ".repeat(n - 1 - i) + "*".repeat(2 * i + 1));
+  }
+  for (let i = 0; i < n; i++) {
+    console.log(" ".repeat(n - 1 - i) + "*".repeat(2 * i + 1));
+  }
+}
+
+main();
