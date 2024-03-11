f7791570592c7ccd4cd33fa6c99b0989ad5b6fcf
LHU <haulrest@gmail.com>
2024-03-11 18:16:50 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 4 ms, Memory: 860 KB -BaekjoonHub

---
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+
+	defer writer.Flush()
+
+	var n int
+	fmt.Fscanln(reader, &n)
+
+	k := int(math.Pow(2, float64(n))) + 1
+	fmt.Fprintln(writer, k*k)
+}
