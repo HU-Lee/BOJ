56754e9b3a3aa216887a19ec9923f116ee36e36a
LHU <haulrest@gmail.com>
2024-04-07 12:37:08 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 4 ms, Memory: 860 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+
+	defer writer.Flush()
+
+	var n int
+	fmt.Fscanln(reader, &n)
+
+	fmt.Fprintln(writer, n*(n-1)*(n-2)/6)
+	fmt.Fprintln(writer, 3)
+}
