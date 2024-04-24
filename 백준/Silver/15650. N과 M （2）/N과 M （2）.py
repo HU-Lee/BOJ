12e5d6f5641b5eb5534cd9ca8dd20f7dd341fd15
Austin Lee <haulrest@gmail.com>
2024-04-24 12:46:56 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,18 @@
+import sys
+
+n,m = map(int,sys.stdin.readline().split())
+arr = []
+
+def dfs(x, l):
+    if l == m:
+        print(' '.join(map(str,arr)))
+        return
+    for i in range(x+1,n+1):
+        arr.append(i)
+        dfs(i,l+1)
+        arr.pop()
+
+for i in range(1,n+1):
+    arr.append(i)
+    dfs(i,1)
+    arr.pop()
