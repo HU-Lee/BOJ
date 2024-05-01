afd0f1fbc02c2ade8856a650dcded406e9c909f6
Austin Lee <haulrest@gmail.com>
2024-05-01 00:41:18 -0700

    [Silver III] Title: N과 M (6), Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -0,0 +1,18 @@
+import sys
+
+n,m = map(int, sys.stdin.readline().split())
+arr = sorted(list(set(map(int, sys.stdin.readline().split()))))
+
+agg = []
+def dfs(i):
+    if i == m:
+        print(' '.join(map(str, agg)))
+        return
+    for j in arr:
+        if agg and agg[-1] >= j:
+            continue
+        agg.append(j)
+        dfs(i + 1)
+        agg.pop()
+
+dfs(0)
